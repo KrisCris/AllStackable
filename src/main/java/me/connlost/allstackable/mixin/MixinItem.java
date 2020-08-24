ec507abf598f030f7ecaab8c6f3140d9ac854794
Pingchuan Huang <hi.imhpc@outlook.com>
2020-08-25 00:44:44 +0800

    fix wrong revert after multiple modifications

--- src/main/java/me/connlost/allstackable/mixin/MixinItem.java
+++ src/main/java/me/connlost/allstackable/mixin/MixinItem.java
@@ -6,6 +6,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Item.class)
 public abstract class MixinItem implements IItemMaxCount {
@@ -24,8 +27,12 @@ public abstract class MixinItem implements IItemMaxCount {
 
     @Override
     public void setMaxCount(int i){
-        this.vanillaMaxCount = maxCount;
         this.maxCount = i;
+    }
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void setVanillaMaxCount(Item.Settings settings, CallbackInfo ci){
+        this.vanillaMaxCount = maxCount;
     }
 
 }
