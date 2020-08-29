200ce7d18a339b84db2d4efdadd2e27b7d9db33c
Conn_Lost <hi.imhpc@outlook.com>
2020-08-29 20:09:53 +0800

    cmd

--- src/main/java/me/connlost/allstackable/server/command/CommandSetMaxCount.java
+++ src/main/java/me/connlost/allstackable/server/command/CommandSetMaxCount.java
@@ -6,6 +6,7 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import me.connlost.allstackable.server.config.ConfigManager;
 import me.connlost.allstackable.util.IItemMaxCount;
+import me.connlost.allstackable.util.ItemsHelper;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.command.argument.ItemStackArgumentType;
 import net.minecraft.item.Item;
@@ -17,20 +18,23 @@ import net.minecraft.util.registry.Registry;
 
 import java.util.Map;
 
-final public class CommandSetMaxCount {
+public class CommandSetMaxCount {
 
+    private static ItemsHelper itemsHelper = ItemsHelper.getItemsHelper();
+    private static ConfigManager configManager = ConfigManager.getConfigManager();
     //TODO re-design commands
     //allstackable
     //->list
     //  ->modified
     //      ->[modifiedCount]
     //  ->vanilla [maxCount]
-    //->modify
+    //->set
     //  ->[item] [count]
     //  ->group
     //      ->vanilla [vanillaMaxCount] [newCount]
     //      ->modified [oldCount] [newCount]
     //      ->all [newCount]
+    //->show [item]
     //->revert
     //  ->[item]
     //  ->group
@@ -38,9 +42,69 @@ final public class CommandSetMaxCount {
     //->revertall
     //
     //
+
+//    public static void nr(){
+//        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+//            dispatcher.register(
+//                    (LiteralArgumentBuilder<ServerCommandSource>) CommandManager.literal("allstack")
+//                    .requires(source -> source.hasPermissionLevel(4))
+//                    .then(CommandManager.literal("list")
+//                        .then(CommandManager.literal("modified")
+//                            .executes(ctx -> listAllModifiedItems(ctx.getSource()))
+//                            .then(CommandManager.argument("count", IntegerArgumentType.integer(1)))
+//                                .executes(ctx -> listModifiedSameCount(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "count"))))
+//                        .then(CommandManager.literal("vanilla")
+//                            .then(CommandManager.argument("count", IntegerArgumentType.integer(1)))
+//                                .executes(ctx -> listVanillaSameCount(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "count")))))
+//                    .then(CommandManager.literal("set")
+//                        .then((RequiredArgumentBuilder) CommandManager.argument("item", ItemStackArgumentType.itemStack())
+//                            .then((RequiredArgumentBuilder) CommandManager.argument("count", IntegerArgumentType.integer(1))
+//                                .executes(ctx -> setMaxCount(
+//                                        ctx.getSource(),
+//                                        ItemStackArgumentType.getItemStackArgument(ctx, "item").getItem(),
+//                                        IntegerArgumentType.getInteger(ctx, "count")))))
+//                            .then(CommandManager.literal("group")
+//                                .then(CommandManager.literal("vanilla")
+//                                    .then(CommandManager.argument("defaultCount", IntegerArgumentType.integer(1))
+//                                        .then(CommandManager.argument("newCount", IntegerArgumentType.integer(1))
+//                                            .executes()))))
+//                    )
+//            );
+//        });
+//    }
+
+    private static int listAllModifiedItems(ServerCommandSource source) {
+        for (Map.Entry<String, Integer> entry : ConfigManager.getConfigManager().getConfigEntry()) {
+            source.sendFeedback(new LiteralText(info(Registry.ITEM.get(new Identifier(entry.getKey())))), false);
+        }
+        return 1;
+    }
+
+    private static int listModifiedSameCount(ServerCommandSource source, int count){
+        //TODO
+        return 1;
+    }
+
+    private static int listVanillaSameCount(ServerCommandSource source, int count){
+        //TODO
+        return 1;
+    }
+
+    private static int setMaxCount(ServerCommandSource source, Item item, int count) {
+        if (count > 64) {
+            source.sendFeedback(new LiteralText("You could only set it up to 64!"), false);
+            return 0;
+        } else {
+            itemsHelper.setSingle(item, count);
+            configManager.addConfig(item.toString(), count);
+            source.sendFeedback(new LiteralText(source.getName() + " has changed the max stack size of " + info(item)), true);
+        }
+        return Command.SINGLE_SUCCESS;
+    }
+
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            dispatcher.register((LiteralArgumentBuilder) ((LiteralArgumentBuilder) CommandManager.literal("allstackable").requires(source -> source.hasPermissionLevel(4))
+            dispatcher.register((LiteralArgumentBuilder<ServerCommandSource>) CommandManager.literal("allstackable").requires(source -> source.hasPermissionLevel(4))
 
                     .then(CommandManager.literal("revertall")
                             .executes(ctx -> revertAll(ctx.getSource()))
@@ -71,7 +135,6 @@ final public class CommandSetMaxCount {
                                     )
                             )
                     )
-                )
             );
         });
     }
@@ -98,20 +161,18 @@ final public class CommandSetMaxCount {
     }
 
     private static String info(Item item){
-        int maxCount = item.getMaxCount();
+        int defaultCount = itemsHelper.getDefaultCount(item);
+        int currentCount = itemsHelper.getCurrentCount(item);
         StringBuilder sb = new StringBuilder();
         sb.append(item.toString())
-                .append(" has maximum stackable count ")
-                .append(item.getMaxCount());
+                .append(" Current: ")
+                .append(currentCount)
+                .append(" Default: ")
+                .append(defaultCount);
         return sb.toString();
     }
 
-    private static int listAllModifiedItems(ServerCommandSource source) {
-        for (Map.Entry<String, Integer> entry : ConfigManager.getConfigManager().getConfigEntry()) {
-            source.sendFeedback(new LiteralText(info(Registry.ITEM.get(new Identifier(entry.getKey())))), false);
-        }
-        return Command.SINGLE_SUCCESS;
-    }
+
 
 
     private static int getMaxCount(ServerCommandSource source, Item item) {
@@ -119,20 +180,7 @@ final public class CommandSetMaxCount {
         return Command.SINGLE_SUCCESS;
     }
 
-    private static int setMaxCount(ServerCommandSource source, Item item, int count) {
-        int originCount = item.getMaxCount();
-        if (count == originCount) {
-            source.sendFeedback(new LiteralText(item.toString() + " unchanged: " + originCount), false);
-        } else if (count > 64) {
-            source.sendFeedback(new LiteralText("You could only set it up to 64!"), false);
-        } else {
 
-            ((IItemMaxCount) item).setMaxCount(count);
-            source.sendFeedback(new LiteralText(source.getName() + " has set stackable count of " + item.toString() + " to " + count), true);
-            ConfigManager.getConfigManager().addConfig(item.toString(), count);
-        }
-        return Command.SINGLE_SUCCESS;
-    }
 
 
 }
