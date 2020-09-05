f55209b021b5d4525d9ad297f57bee60c6aaed44
Conn_Lost <hi.imhpc@outlook.com>
2020-09-05 10:19:30 +0800

    fix finishing milk

---
+++ src/main/java/me/connlost/allstackable/mixin/MixinMilkBucketItem.java
@@ -0,0 +1,30 @@
+package me.connlost.allstackable.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.MilkBucketItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MilkBucketItem.class)
+public class MixinMilkBucketItem {
+
+    @Inject(method = "finishUsing", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/item/ItemStack;decrement(I)V"), cancellable = true)
+    private void stackableMilkBucket(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir){
+        if (!stack.isEmpty()){
+            if (!world.isClient)
+                user.clearStatusEffects();
+            if (user instanceof PlayerEntity){
+                ((PlayerEntity)user).inventory.insertStack(new ItemStack(Items.BUCKET));
+                cir.setReturnValue(stack);
+            }
+        }
+
+
+    }
+}
