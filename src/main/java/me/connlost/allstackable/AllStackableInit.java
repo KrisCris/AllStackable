f7d5d8d279fd2d758b818b5517f7c02fbdc06702
Conn_Lost <hi.imhpc@outlook.com>
2020-12-22 16:04:14 +0800

    implemented fabric api for server registration

--- src/main/java/me/connlost/allstackable/AllStackableInit.java
+++ src/main/java/me/connlost/allstackable/AllStackableInit.java
@@ -1,6 +1,8 @@
 package me.connlost.allstackable;
 
+import me.connlost.allstackable.server.Server;
 import me.connlost.allstackable.server.command.StackSizeCommand;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.fabricmc.api.ModInitializer;
@@ -15,9 +17,11 @@ public class AllStackableInit implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOG.info("[All Stackable] Start loading!");
-
 		StackSizeCommand.register();
 		LOG.info("[All Stackable] Command registered.");
+		ServerLifecycleEvents.SERVER_STARTED.register(minecraftServer -> {
+			Server.onServerLoaded(minecraftServer);
+		});
 	}
 
 
