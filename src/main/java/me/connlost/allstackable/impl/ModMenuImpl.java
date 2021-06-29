89d5b2283e6dbe6dd3a46cf50ac3c8dea5225cdf
Conn_Lost <hi.imhpc@outlook.com>
2021-06-29 23:41:32 +0800

    delay work on this part..

--- src/main/java/me/connlost/allstackable/impl/ModMenuImpl.java
+++ src/main/java/me/connlost/allstackable/impl/ModMenuImpl.java
@@ -1,12 +1,11 @@
-package me.connlost.allstackable.impl;
-
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-
+//package me.connlost.allstackable.impl;
+//
+//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+//import com.terraformersmc.modmenu.api.ModMenuApi;
+//
 //public class ModMenuImpl implements ModMenuApi {
-////    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-////        return parent -> {
-////            // Return the screen here with the one you created from Cloth Config Builder
-////        };
-////    }
+//    @Override
+//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//        return ClothScreenImpl::getConfigScreen;
+//    }
 //}
