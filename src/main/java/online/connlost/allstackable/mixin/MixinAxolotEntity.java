07ddebab23a9424d1848313dee3a889ea3eefdaf
connlost <hi.imhpc@outlook.com>
2021-12-30 04:41:55 -0600

    change injection point of axolot mixin

--- src/main/java/online/connlost/allstackable/mixin/MixinAxolotEntity.java
+++ src/main/java/online/connlost/allstackable/mixin/MixinAxolotEntity.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AxolotlEntity.class)
 public class MixinAxolotEntity {
-    @Inject(method = "eat", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "eat", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setStackInHand(Lnet/minecraft/util/Hand;Lnet/minecraft/item/ItemStack;)V"), cancellable = true)
     protected void reduceBucketsByOne(PlayerEntity player, Hand hand, ItemStack stack, CallbackInfo ci){
         if (ItemsHelper.isModified(stack) && stack.getCount() > 1){
             if (player.getAbilities().creativeMode) {
