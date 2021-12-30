221f43b6b43591a8e72e12c026196e12f9aa8f18
connlost <hi.imhpc@outlook.com>
2021-12-30 05:18:26 -0600

    only activate when the item is modified

--- src/main/java/online/connlost/allstackable/mixin/MixinAbstractFurnaceBlockEntity.java
+++ src/main/java/online/connlost/allstackable/mixin/MixinAbstractFurnaceBlockEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.item.Items;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import online.connlost.allstackable.util.ItemsHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,8 +18,10 @@ public class MixinAbstractFurnaceBlockEntity {
     @Inject(method = "tick", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/item/ItemStack;decrement(I)V"))
     private static void popBuckets(World world, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity blockEntity, CallbackInfo ci){
         ItemStack itemStack = (ItemStack)((AccessorFurnaceInventory)blockEntity).getInventory().get(1);
-        if (itemStack.isOf(Items.LAVA_BUCKET)){
-            ItemScatterer.spawn(world, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), new ItemStack(Items.BUCKET));
+        if(ItemsHelper.isModified(itemStack)) {
+            if (itemStack.isOf(Items.LAVA_BUCKET)){
+                ItemScatterer.spawn(world, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), new ItemStack(Items.BUCKET));
+            }
         }
     }
 }
